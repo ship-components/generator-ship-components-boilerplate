0794ba6f9b716b81441639a846db4aa75cc8bc61
Sepand Assadi <sepand.assadi@sony.com>
2017-09-11 16:36:10 -0700

    Final fix on copy folders properly

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,6 @@ function generator() {
   const yosay = require('yosay');
   const path = require('path');
   const mkdirp = require('mkdirp');
-  const fs = require('fs-extra');
 
   module.exports = yeoman.extend({
     prompting: function() {
@@ -40,14 +39,6 @@ function generator() {
         mkdirp(this.props.projectName);
         this.destinationRoot(this.destinationPath(this.props.projectName));
       }
-
-      //copy all files
-      let source = this.sourceRoot() + '/resource';
-      let destination = this.destinationRoot();
-
-      fs.copy(source, destination)
-        .then(() => console.log('success!'))
-        .catch(err => console.error(err));
     },
     writing: function() {
       // package
@@ -63,7 +54,26 @@ function generator() {
       //package.json
       this.fs.writeJSON(this.destinationPath('package.json'), pkg);
 
-      //copy templates
+      // create all required folders
+      mkdirp(this.destinationRoot() + '/' + 'dist');
+      mkdirp(this.destinationRoot() + '/' + 'examples');
+      mkdirp(this.destinationRoot() + '/' + 'src');
+      mkdirp(this.destinationRoot() + '/' + 'tasks');
+
+      // create src sub folders
+      mkdirp(this.destinationRoot() + '/' + 'src/__tests__');
+      mkdirp(this.destinationRoot() + '/' + 'src/__tests__/__snapshots__');
+
+      // create tasks folders
+      mkdirp(this.destinationRoot() + '/' + 'tasks');
+      mkdirp(this.destinationRoot() + '/' + 'tasks/config');
+      mkdirp(this.destinationRoot() + '/' + 'tasks/register');
+
+      //copy all files
+      let source = this.sourceRoot() + '/resource';
+      let destination = this.destinationRoot();
+
+      //copy app templates
       this.fs.copy(this.sourceRoot() + '/' + 'babelrc_tmpl', '.babelrc');
       this.fs.copy(this.sourceRoot() + '/' + 'coveralls_tmpl', '.coveralls.yml');
       this.fs.copy(this.sourceRoot() + '/' + 'eslintrc_tmpl', '.eslintrc');
@@ -71,6 +81,30 @@ function generator() {
       this.fs.copy(this.sourceRoot() + '/' + 'gruntfile_tmpl', 'Gruntfile.js');
       this.fs.copy(this.sourceRoot() + '/' + 'npmignore_tmpl', '.npmignore');
       this.fs.copy(this.sourceRoot() + '/' + 'travis_tmpl', '.travis.yml');
+
+      // copy examples folder
+      this.fs.copy(source + '/examples/index.html', destination + '/examples/index.html');
+      this.fs.copy(source + '/examples/index.jsx', destination + '/examples/index.jsx');
+
+      // copy src folder
+      this.fs.copy(source + '/src/index.css', destination + '/src/index.css');
+      this.fs.copy(source + '/src/index.js', destination + '/src/index.js');
+      this.fs.copy(source + '/src/__tests__/index.test.js', destination + '/src/__tests__/index.test.js');
+      this.fs.copy(source + '/src/__tests__/__snapshots__/index.test.js.snap', destination + '/src/__tests__/__snapshots__/index.test.js.snap');
+
+      // Copy tasks folder
+      // Config folder
+      this.fs.copy(source + '/tasks/config/clean.js', destination + '/tasks/config/clean.js');
+      this.fs.copy(source + '/tasks/config/coveralls.js', destination + '/tasks/config/coveralls.js');
+      this.fs.copy(source + '/tasks/config/eslint.js', destination + '/tasks/config/eslint.js');
+      this.fs.copy(source + '/tasks/config/webpack.config.js', destination + '/tasks/config/webpack.config.js');
+      this.fs.copy(source + '/tasks/config/webpack.js', destination + '/tasks/config/webpack.js');
+      // register folder
+      this.fs.copy(source + '/tasks/register/build.js', destination + '/tasks/register/build.js');
+      this.fs.copy(source + '/tasks/register/coveralls.js', destination + '/tasks/register/coveralls.js');
+      this.fs.copy(source + '/tasks/register/default.js', destination + '/tasks/register/default.js');
+      this.fs.copy(source + '/tasks/register/lint.js', destination + '/tasks/register/lint.js');
+      this.fs.copy(source + '/tasks/register/server.js', destination + '/tasks/register/server.js');
     },
     install: function() {
       process.exit();
