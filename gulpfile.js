9d640f62f4ee1646ad0cfaf2400231f15db419ac
Sam Assadi <samsassadi@gmail.com>
2019-06-10 14:21:35 -0700

    updates to latest dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,33 @@
 'use strict';
-const path = require('path');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 // const excludeGitignore = require('gulp-exclude-gitignore');
 const mocha = require('gulp-mocha');
 const istanbul = require('gulp-istanbul');
-const nsp = require('gulp-nsp');
 const plumber = require('gulp-plumber');
 
 gulp.task('eslint', function() {
   return gulp.src('generators/**/*.js')
     .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(eslint({
-      useEslintrc: true
+      presets: ['@babel/preset-env']
     }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', function(cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
-
-gulp.task('pre-test', function() {
+gulp.task('pre-test', gulp.series(function() {
   return gulp.src('generators/**/*.js')
-    .pipe(babel({
-      presets: ['env']
-    }))
     .pipe(istanbul({
       includeUntested: true
     }))
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
     .pipe(istanbul.hookRequire());
-});
+}));
 
-gulp.task('test', ['pre-test'], function(cb) {
+gulp.task('test', gulp.series('pre-test', function(cb) {
   let mochaErr;
 
   gulp.src('test/**/*.js')
@@ -49,11 +40,9 @@ gulp.task('test', ['pre-test'], function(cb) {
     .on('end', function() {
       cb(mochaErr);
     });
-});
-
-gulp.task('watch', function() {
+}));
+gulp.task('watch', gulp.series(function() {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
-});
+}));
 
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['eslint', 'test']);
+gulp.task('default', gulp.series('eslint', 'test'));
